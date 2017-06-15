196e39f3a4aa85ae605ad041b7699f8741596534
Vadim Sannikov <vsj.vadim@gmail.com>
2017-06-16 03:33:30 +0400

    Improve autoloader

--- autoload.php
+++ autoload.php
@@ -4,7 +4,8 @@
  *
  * @author Vadim Sannikov <vsj.vadim@gmail.com>
  */
+declare(strict_types=1);
 
-spl_autoload_register(function ($name) {
+spl_autoload_register(function (string $name): void {
   require_once __DIR__ . '/' . str_replace('\\', '/', ltrim($name, '\\')) . '.php';
 });
