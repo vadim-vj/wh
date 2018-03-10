1649562999b9b225ae54b2e94a60d3adebc9f20f
Vadim Sannikov <vsj.vadim@gmail.com>
2018-03-11 03:16:19 +0400

    Further changes

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 'use strict';
 
 class MenuItem {
-  constructor(title, href) {
+  constructor(title, href, child) {
     this.title = title;
     this.href = href;
+    this.child = child || '';
   }
 
   toString() {
-    return '<a href="' + (this.href ? 'page/' + this.href + '.html' : '') + '">' + this.title + '</a>';
+    return '<a href="' + (this.href ? 'page/' + this.href + '.html' : '') + '">' + this.title + '</a>' + this.child;
   }
 }
 
 class Menu {
-  get items() {
-    return [
-      new MenuItem('Главная', ''),
-      new MenuItem('Литература', 'literature'),
-      new MenuItem('Проекты', 'projects'),
-      new MenuItem('Словарь', 'dictionary'),
-    ];
+  constructor(items) {
+    this.items = items;
   }
 
   toString() {
@@ -29,7 +25,15 @@ class Menu {
 document.addEventListener('DOMContentLoaded', event => {
   document.body.innerHTML = '<div class="main">\n<h1>' + document.title + '</h1>' + document.body.innerHTML.trim() + '</div>';
 
-  const menu = new Menu();
+  const menu = new Menu([
+    new MenuItem('Главная', ''),
+    new MenuItem('Литература', 'literature'),
+    new MenuItem('Проекты', 'projects', new Menu([
+      new MenuItem('OpenWorm', 'projects/openworm'),
+      new MenuItem('Numenta', 'projects/numenta'),
+    ])),
+    new MenuItem('Словарь', 'dictionary'),
+  ]);
   document.body.insertAdjacentHTML('afterbegin', '<div class="left">' + menu + '</div>');
 
   const divMenu = document.body.querySelector('div.main > div.menu');
