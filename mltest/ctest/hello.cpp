34c1c0a7e1d4618a05ed04f6194c7f094f272eb7
Vadim Sannikov <vsj.vadim@gmail.com>
2018-10-29 23:43:33 +0400

    Further changes

---
+++ mltest/ctest/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ *
+ */
+int main()
+{
+    std::cout << "Enter two numbers: " << std::endl;
+    int vl = 0, v2 = 0;
+    std::cin >> vl >> v2;
+    std::cout << "The sum of " << vl << " and " << v2 << " is " << vl + v2 << std::endl;
+    std::cout << /* "*/" /* "/*" */;
+    return 0;
+}
