26c45f808b3c5419955aec3f0f2ff4682f381b3a
Vadim Sannikov <vsj.vadim@gmail.com>
2019-06-06 05:24:19 +0400

    Trailing whitespace removed

--- common/footer.php
+++ common/footer.php
@@ -13,7 +13,7 @@ if (!defined('BASE_DIR')) {
     throw new \LogicException('Footer: the "top.inc.php" file is not included.');
 }
 
-echo EOL . 'Local directory: ' . (IS_CLI ? '' : '<a href="' . LOCAL_URL . '" target="_blank">') 
+echo EOL . 'Local directory: ' . (IS_CLI ? '' : '<a href="' . LOCAL_URL . '" target="_blank">')
     . getRelativeDirPath() . (IS_CLI ? '' : '</a>');
 echo EOL . 'GitHub directory: ' . (IS_CLI ? '' : '<a href="' . GITHUB_URL . '" target="_blank">')
     . GITHUB_URL . (IS_CLI ? '' : '</a>') . EOL;
