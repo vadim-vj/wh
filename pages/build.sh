69fafa79defcb46b378a8926119f7cbb92863035
Vadim Sannikov <vsj.vadim@gmail.com>
2019-09-30 03:38:44 +0400

    Save

--- pages/build.sh
+++ pages/build.sh
@@ -9,6 +9,9 @@ else
   POSTS_PATH=posts
   POST_HTML_INTENDED=temp.html
   INTEND_PATTER='s/^/    /'
+  declare -A TITLES
+  declare -A DATES
+  declare -a GENERATED
 
   . ./tags.sh
 
@@ -17,7 +20,8 @@ else
       POST_MD=$d/index.md
       POST_HTML=$d/index.html
       POST_SETTINGS=$d/settings.sh
-      GENERATED_HTML=$BUILD_PATH/`basename $d`.html
+      GENERATED_NAME=`basename $d`.html
+      GENERATED_HTML=$BUILD_PATH/$GENERATED_NAME
 
       if [ -s "$POST_HTML" ]; then
         sed "$INTEND_PATTER" $POST_HTML > $POST_HTML_INTENDED
@@ -36,15 +40,42 @@ else
       title=
       date=
 
+
       if [ -f "$POST_SETTINGS" ]; then
         . $d/settings.sh
+
+        if [ "$GENERATED_NAME" == "index.html" ]; then
+          INDEX_FILE=$GENERATED_HTML
+          INDEX_TITLE=$title
+
+        else
+          TITLES[$GENERATED_NAME]=$title
+          DATES[$GENERATED_NAME]=$date
+        fi
       fi
 
       sed -i "s/####TITLE####/$title/" $GENERATED_HTML
-      sed -i "s/####DATE####/`LC_TIME="C.UTF-8" date --date="$date" "+%a %d %b %Y, %R"`/" $GENERATED_HTML
+      sed -i "s/####DATE####/`[[ $date ]] && LC_TIME="C.UTF-8" date --date="$date" "+%a %d %b %Y, %R"`/" $GENERATED_HTML
+
+      GENERATED+=($GENERATED_HTML)
     fi
   done
 
+  # TODO: sort DATES
+
+  truncate -s0 $POST_HTML_INTENDED
+  for KEY in "${!DATES[@]}"; do
+    echo "<li><a href=\"$KEY\">${TITLES[$KEY]}</a></li>" >> $POST_HTML_INTENDED
+  done
+
+  sed -i "$INTEND_PATTER" $POST_HTML_INTENDED
+  for FILE in "${GENERATED[@]}"; do
+    sed -i "/<ul id=\"nav\">/r$POST_HTML_INTENDED" $FILE
+    sed -i "s/####MAIN_TITLE####/$INDEX_TITLE/" $FILE
+  done
+
+  sed -i 's/^/  /' $POST_HTML_INTENDED
+  sed -i "/<ul id=\"nav-main\">/r$POST_HTML_INTENDED" ${INDEX_FILE}
+
   rm $POST_HTML_INTENDED
 fi
-
