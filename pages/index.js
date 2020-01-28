d86e50ce36e32a3b11eddcc5f68404b35608b0b3
Vadim Sannikov <vsj.vadim@gmail.com>
2020-01-29 01:19:16 +0400

    Further changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  for (const link of document.querySelectorAll('#content > ul:not(#nav-main) a')) {
-    link.setAttribute('target', '_blank');
-  }
-});
+for (const link of document.querySelectorAll('main > article > :not(nav) a')) {
+  link.setAttribute('target', '_blank');
+  link.setAttribute('rel', 'noopener');
+}
