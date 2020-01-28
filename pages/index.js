52b1e07453e1398218a27bbfa3016c5b4608cfce
Vadim Sannikov <vsj.vadim@gmail.com>
2020-01-29 02:31:37 +0400

    Further changes

--- pages/index.js
+++ pages/index.js
@@ -2,3 +2,7 @@ for (const link of document.querySelectorAll('main > article > :not(nav) a')) {
   link.setAttribute('target', '_blank');
   link.setAttribute('rel', 'noopener');
 }
+
+for (const link of document.querySelectorAll('link[rel="preload"]')) {
+  link.setAttribute('rel', 'stylesheet');
+}
