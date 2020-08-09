b729786f1bbe3bacce8bf6d317e2a18aaf30e67a
Vadim Sannikov <vsj.vadim@gmail.com>
2020-08-09 17:23:55 +0400

    [Syll] Python; interview quests. stub

---
+++ syllabuses/python/interview/_.mm
@@ -0,0 +1,158 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Python" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_150556264" CREATED="1596846583932" MODIFIED="1596846635391"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Numbered" COLOR="#000000" STYLE="fork" NUMBERED="true">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode TEXT="Processed" COLOR="#000000" BACKGROUND_COLOR="#ccff66" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="26" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Общее" POSITION="right" ID="ID_492818217" CREATED="1596945705595" MODIFIED="1596945709282">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Командная строка и среда" POSITION="right" ID="ID_1506170337" CREATED="1596846787638" MODIFIED="1596846794900">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Лексическая структура" POSITION="right" ID="ID_1297010259" CREATED="1596846837697" MODIFIED="1596846847471">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Code style" POSITION="right" ID="ID_1922790604" CREATED="1596847473930" MODIFIED="1596847482105">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Типы" POSITION="right" ID="ID_1958726856" CREATED="1596846847951" MODIFIED="1596846850572">
+<edge COLOR="#00ff00"/>
+<node TEXT="Простые" ID="ID_547305101" CREATED="1596846904481" MODIFIED="1596846909657"/>
+<node TEXT="Коллекции" ID="ID_978605026" CREATED="1596846909938" MODIFIED="1596846912788">
+<node TEXT="Встроенные" ID="ID_1890375357" CREATED="1596847169744" MODIFIED="1596847172462"/>
+<node TEXT="Из библиотеки" ID="ID_1954695286" CREATED="1596847172735" MODIFIED="1596847185481"/>
+</node>
+</node>
+<node TEXT="Управляющие структуры" POSITION="right" ID="ID_609866299" CREATED="1596846884648" MODIFIED="1596846890872">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Функции" POSITION="right" ID="ID_244211989" CREATED="1596846891380" MODIFIED="1596846894206">
+<edge COLOR="#00ffff"/>
+<node TEXT="Итераторы" ID="ID_162812011" CREATED="1596847035380" MODIFIED="1596847037881"/>
+<node TEXT="Генераторы" ID="ID_1964096847" CREATED="1596847038318" MODIFIED="1596847041653"/>
+<node TEXT="Декораторы" ID="ID_428361426" CREATED="1596847071545" MODIFIED="1596847074071"/>
+</node>
+<node TEXT="Классы" POSITION="right" ID="ID_221335544" CREATED="1596846937387" MODIFIED="1596846943138">
+<edge COLOR="#7c0000"/>
+<node TEXT="Метаклассы" ID="ID_266428921" CREATED="1596847076074" MODIFIED="1596847081324"/>
+<node TEXT="Перегрузка и магические методы" ID="ID_1820839129" CREATED="1596847091515" MODIFIED="1596847097965"/>
+<node TEXT="Атрибуты и их разрешение" ID="ID_409612165" CREATED="1596847098404" MODIFIED="1596847113046"/>
+</node>
+<node TEXT="Исключения" POSITION="right" ID="ID_1413180238" CREATED="1596846962377" MODIFIED="1596846965206">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Строки и байты" POSITION="right" ID="ID_293335505" CREATED="1596846981107" MODIFIED="1596846984845">
+<edge COLOR="#007c7c"/>
+<node TEXT="Регулярные выражения" ID="ID_1855968497" CREATED="1596847043830" MODIFIED="1596847050355"/>
+</node>
+<node TEXT="Пакеты и модули" POSITION="right" ID="ID_762491352" CREATED="1596846944341" MODIFIED="1596846949356">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Ввод/вывод, файлы" POSITION="right" ID="ID_1617764251" CREATED="1596847124555" MODIFIED="1596847134375">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Тестирование" POSITION="right" ID="ID_1739469748" CREATED="1596846949710" MODIFIED="1596846952778">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Параллельное программирование" POSITION="right" ID="ID_492520488" CREATED="1596847208539" MODIFIED="1596847224555">
+<edge COLOR="#0000ff"/>
+<node TEXT="Потоки/процессы" ID="ID_1044352138" CREATED="1596847226693" MODIFIED="1596847231980"/>
+<node TEXT="Asyncio" ID="ID_492409217" CREATED="1596847232615" MODIFIED="1596847236207"/>
+</node>
+<node TEXT="Стандартная библиотека" POSITION="right" ID="ID_73665629" CREATED="1596847155411" MODIFIED="1596847164775">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Сравнение версий 2 и 3" POSITION="right" ID="ID_603812188" CREATED="1596847279012" MODIFIED="1596847287778">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Фреймворки" POSITION="right" ID="ID_1810002710" CREATED="1596847789071" MODIFIED="1596847803606">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Задачи и код" POSITION="right" ID="ID_1781350956" CREATED="1596945941971" MODIFIED="1596945947290">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="https://habr.com/ru/post/439576/" POSITION="left" ID="ID_273742804" CREATED="1596946186524" MODIFIED="1596946250313" LINK="https://habr.com/ru/post/439576/">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="https://kirill-sklyarenko.ru/lenta/150-voprosov-na-sobesedovanie-python-bez-opyta" POSITION="left" ID="ID_1402471884" CREATED="1596946267228" MODIFIED="1596946271813" LINK="https://kirill-sklyarenko.ru/lenta/150-voprosov-na-sobesedovanie-python-bez-opyta">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="https://proglib.io/p/python-interview/" POSITION="left" ID="ID_1991048835" CREATED="1596946330853" MODIFIED="1596946338012" LINK="https://proglib.io/p/python-interview/">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="https://www.severcart.ru/blog/all/essential-python-interview-questions/" POSITION="left" ID="ID_423055610" CREATED="1596946351270" MODIFIED="1596946356199" LINK="https://www.severcart.ru/blog/all/essential-python-interview-questions/">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="https://proglib.io/p/20-questions-for-python-developer" POSITION="left" ID="ID_775641010" CREATED="1596946367395" MODIFIED="1596946371600" LINK="https://proglib.io/p/20-questions-for-python-developer">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="https://grishaev.me/interview/" POSITION="left" ID="ID_849829736" CREATED="1596946383144" MODIFIED="1596946387544" LINK="https://grishaev.me/interview/">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
