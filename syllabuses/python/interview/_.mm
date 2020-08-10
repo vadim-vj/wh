ae44551706519792aa5c7be59f9552f678c16172
Vadim Sannikov <vsj.vadim@gmail.com>
2020-08-10 05:49:54 +0400

    [Syll] Python; interview quests., code style

--- syllabuses/python/interview/_.mm
+++ syllabuses/python/interview/_.mm
@@ -33,10 +33,13 @@
 <stylenode LOCALIZED_TEXT="styles.important">
 <icon BUILTIN="yes"/>
 </stylenode>
-<stylenode TEXT="Numbered" COLOR="#000000" STYLE="fork" NUMBERED="true">
+<stylenode TEXT="Numbered" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="fork" NUMBERED="true">
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode TEXT="Processed" COLOR="#000000" BACKGROUND_COLOR="#ccff66" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode TEXT="Numbered_processed" COLOR="#000000" BACKGROUND_COLOR="#ccff66" STYLE="fork" NUMBERED="true">
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 </stylenode>
@@ -70,6 +73,7 @@
 <hook NAME="AutomaticEdgeColor" COUNTER="26" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Общее" POSITION="right" ID="ID_492818217" CREATED="1596945705595" MODIFIED="1596945709282">
 <edge COLOR="#00007c"/>
+<node TEXT="Что такое garbage collector (gc)? В чём его плюсы и минусы?" STYLE_REF="Numbered" ID="ID_1687540259" CREATED="1597014315588" MODIFIED="1597014327347" LINK="https://habr.com/ru/post/417215/"/>
 </node>
 <node TEXT="Командная строка и среда" POSITION="right" ID="ID_1506170337" CREATED="1596846787638" MODIFIED="1596846794900">
 <edge COLOR="#ff0000"/>
@@ -77,31 +81,57 @@
 <node TEXT="Лексическая структура" POSITION="right" ID="ID_1297010259" CREATED="1596846837697" MODIFIED="1596846847471">
 <edge COLOR="#0000ff"/>
 </node>
-<node TEXT="Code style" POSITION="right" ID="ID_1922790604" CREATED="1596847473930" MODIFIED="1596847482105">
+<node TEXT="Code style `[style]`" STYLE_REF="Processed" POSITION="right" ID="ID_1922790604" CREATED="1596847473930" MODIFIED="1597024024014">
 <edge COLOR="#ff00ff"/>
+<node TEXT="Что такое PEP8?" STYLE_REF="Numbered_processed" ID="ID_645033637" CREATED="1597012553287" MODIFIED="1597024017654"/>
+<node TEXT="Назовите основные моменты PEP8" STYLE_REF="Numbered_processed" ID="ID_1451316872" CREATED="1597017756210" MODIFIED="1597024017669"/>
+<node TEXT="Какие есть еще документы по code style, помимо PEP8?" STYLE_REF="Numbered_processed" ID="ID_1837328317" CREATED="1597016183869" MODIFIED="1597024017681"/>
+<node TEXT="Какие вы знаете инструменты для управления стилем кода? Приведите их краткий обзор" STYLE_REF="Numbered_processed" ID="ID_1531565074" CREATED="1597015256622" MODIFIED="1597024017689"/>
+<node TEXT="Как вы следуете PEP8 и стилю кода вообще?" STYLE_REF="Numbered_processed" ID="ID_919189250" CREATED="1597015263939" MODIFIED="1597024017693"/>
 </node>
 <node TEXT="Типы" POSITION="right" ID="ID_1958726856" CREATED="1596846847951" MODIFIED="1596846850572">
 <edge COLOR="#00ff00"/>
 <node TEXT="Простые" ID="ID_547305101" CREATED="1596846904481" MODIFIED="1596846909657"/>
 <node TEXT="Коллекции" ID="ID_978605026" CREATED="1596846909938" MODIFIED="1596846912788">
-<node TEXT="Встроенные" ID="ID_1890375357" CREATED="1596847169744" MODIFIED="1596847172462"/>
+<node TEXT="Встроенные" ID="ID_1890375357" CREATED="1596847169744" MODIFIED="1596847172462">
+<node TEXT="Какие вы знаете структуры данных? Какие из них являются mutable/immutable?" STYLE_REF="Numbered" ID="ID_734698186" CREATED="1597014019257" MODIFIED="1597014026663"/>
+<node TEXT="Где будет быстрее поиск, а где перебор и почему: `dict`, `list`, `set`, `tuple`?" STYLE_REF="Numbered" ID="ID_456666505" CREATED="1597014038599" MODIFIED="1597014040968"/>
+</node>
 <node TEXT="Из библиотеки" ID="ID_1954695286" CREATED="1596847172735" MODIFIED="1596847185481"/>
 </node>
 </node>
 <node TEXT="Управляющие структуры" POSITION="right" ID="ID_609866299" CREATED="1596846884648" MODIFIED="1596846890872">
 <edge COLOR="#ff00ff"/>
+<node TEXT="Что такое list/dict comprehension?" STYLE_REF="Numbered" ID="ID_80719452" CREATED="1597014132739" MODIFIED="1597014137474"/>
 </node>
 <node TEXT="Функции" POSITION="right" ID="ID_244211989" CREATED="1596846891380" MODIFIED="1596846894206">
 <edge COLOR="#00ffff"/>
+<node TEXT="Как передаются значения аргументов в функцию или метод?" STYLE_REF="Numbered" ID="ID_385947043" CREATED="1597014066132" MODIFIED="1597014074533"/>
 <node TEXT="Итераторы" ID="ID_162812011" CREATED="1596847035380" MODIFIED="1596847037881"/>
-<node TEXT="Генераторы" ID="ID_1964096847" CREATED="1596847038318" MODIFIED="1596847041653"/>
-<node TEXT="Декораторы" ID="ID_428361426" CREATED="1596847071545" MODIFIED="1596847074071"/>
+<node TEXT="Генераторы" ID="ID_1964096847" CREATED="1596847038318" MODIFIED="1597014121194" LINK="https://xakep.ru/2014/10/06/generatora-iteratory-python/">
+<node TEXT="Что такое генератор?" STYLE_REF="Numbered" ID="ID_1654636388" CREATED="1597014083848" MODIFIED="1597014097702"/>
+<node TEXT="Чем генератор отличается от итератора?" STYLE_REF="Numbered" ID="ID_340146127" CREATED="1597014099365" MODIFIED="1597014106552"/>
+</node>
+<node TEXT="Декораторы" ID="ID_428361426" CREATED="1596847071545" MODIFIED="1596847074071">
+<node TEXT="Что такое декоратор?" STYLE_REF="Numbered" ID="ID_1418787976" CREATED="1597012856092" MODIFIED="1597012874092"/>
+<node TEXT="Для чего могут использоваться? Приведите примеры" STYLE_REF="Numbered" ID="ID_1407991525" CREATED="1597013073137" MODIFIED="1597013086060"/>
+<node TEXT="Напишите пример декоратора" STYLE_REF="Numbered" ID="ID_1552377725" CREATED="1597013063937" MODIFIED="1597013071197"/>
+</node>
 </node>
 <node TEXT="Классы" POSITION="right" ID="ID_221335544" CREATED="1596846937387" MODIFIED="1596846943138">
 <edge COLOR="#7c0000"/>
 <node TEXT="Метаклассы" ID="ID_266428921" CREATED="1596847076074" MODIFIED="1596847081324"/>
+<node TEXT="Именование и доступ" ID="ID_440237296" CREATED="1597013917774" MODIFIED="1597013924923">
+<node TEXT="Как реализованы public, private, static методы?" STYLE_REF="Numbered" ID="ID_1245377289" CREATED="1597013926286" MODIFIED="1597013936192"/>
+<node TEXT="Интроспекция (использование dir(), dir, hasattr(), getattr()), name mangling" STYLE_REF="Numbered" ID="ID_1198045816" CREATED="1597014152969" MODIFIED="1597014155746"/>
+<node TEXT="Какая разница между одинарным (`_`) и двойным (`__`) подчеркиванием?" STYLE_REF="Numbered" ID="ID_1534474041" CREATED="1597014164276" MODIFIED="1597014180066" LINK="https://hackernoon.com/understanding-the-underscore-of-python-309d1a029edc"/>
+</node>
 <node TEXT="Перегрузка и магические методы" ID="ID_1820839129" CREATED="1596847091515" MODIFIED="1596847097965"/>
 <node TEXT="Атрибуты и их разрешение" ID="ID_409612165" CREATED="1596847098404" MODIFIED="1596847113046"/>
+<node TEXT="Наследование" ID="ID_253776720" CREATED="1597014216490" MODIFIED="1597014224581">
+<node TEXT="Что такое MRO? Какая разница между MRO2 и MR3 (diamond problem)?" STYLE_REF="Numbered" ID="ID_1265689721" CREATED="1597014225653" MODIFIED="1597014230066"/>
+<node TEXT="Что такое и чем отличается old-style от new-style classes?" STYLE_REF="Numbered" ID="ID_1135430447" CREATED="1597014239575" MODIFIED="1597014255295"/>
+</node>
 </node>
 <node TEXT="Исключения" POSITION="right" ID="ID_1413180238" CREATED="1596846962377" MODIFIED="1596846965206">
 <edge COLOR="#7c007c"/>
@@ -118,23 +148,34 @@
 </node>
 <node TEXT="Тестирование" POSITION="right" ID="ID_1739469748" CREATED="1596846949710" MODIFIED="1596846952778">
 <edge COLOR="#007c00"/>
+<node TEXT="Как вы тестируете код? Что такое mocking?" STYLE_REF="Numbered" ID="ID_517705142" CREATED="1597014000252" MODIFIED="1597014004414"/>
 </node>
 <node TEXT="Параллельное программирование" POSITION="right" ID="ID_492520488" CREATED="1596847208539" MODIFIED="1596847224555">
 <edge COLOR="#0000ff"/>
-<node TEXT="Потоки/процессы" ID="ID_1044352138" CREATED="1596847226693" MODIFIED="1596847231980"/>
-<node TEXT="Asyncio" ID="ID_492409217" CREATED="1596847232615" MODIFIED="1596847236207"/>
+<node TEXT="Что такое GIL? Какие у него есть проблемы?" STYLE_REF="Numbered" ID="ID_266865387" CREATED="1597014200287" MODIFIED="1597014207008"/>
+<node TEXT="Потоки/процессы" ID="ID_1044352138" CREATED="1596847226693" MODIFIED="1596847231980">
+<node TEXT="Что вы знаете о Threading. Threading vs Multiprocessing?" ID="ID_388509528" CREATED="1597014256474" MODIFIED="1597014290346" LINK="http://effbot.org/zone/thread-synchronization.htm"/>
+</node>
+<node TEXT="Asyncio" ID="ID_492409217" CREATED="1596847232615" MODIFIED="1596847236207">
+<node TEXT="Работали ли вы с asyncio? В чём его особенность?" STYLE_REF="Numbered" ID="ID_1852140909" CREATED="1597014297921" MODIFIED="1597014301872"/>
+<node TEXT="Что такое async/await, для чего они нужны и как их использовать?" STYLE_REF="Numbered" ID="ID_280858800" CREATED="1597014336671" MODIFIED="1597014338061"/>
+</node>
 </node>
 <node TEXT="Стандартная библиотека" POSITION="right" ID="ID_73665629" CREATED="1596847155411" MODIFIED="1596847164775">
 <edge COLOR="#ff0000"/>
 </node>
 <node TEXT="Сравнение версий 2 и 3" POSITION="right" ID="ID_603812188" CREATED="1596847279012" MODIFIED="1596847287778">
 <edge COLOR="#00ff00"/>
+<node TEXT="Перечислите принципиалльные отличия" STYLE_REF="Numbered" ID="ID_1329197024" CREATED="1597013943853" MODIFIED="1597013959587"/>
 </node>
 <node TEXT="Фреймворки" POSITION="right" ID="ID_1810002710" CREATED="1596847789071" MODIFIED="1596847803606">
 <edge COLOR="#7c0000"/>
 </node>
 <node TEXT="Задачи и код" POSITION="right" ID="ID_1781350956" CREATED="1596945941971" MODIFIED="1596945947290">
 <edge COLOR="#007c00"/>
+<node TEXT="Паттерны" ID="ID_877093789" CREATED="1597012810672" MODIFIED="1597012815965">
+<node TEXT="Варианты реализации шаблона Singleton" STYLE_REF="Numbered" ID="ID_1019351248" CREATED="1597012827373" MODIFIED="1597012839102"/>
+</node>
 </node>
 <node TEXT="https://habr.com/ru/post/439576/" POSITION="left" ID="ID_273742804" CREATED="1596946186524" MODIFIED="1596946250313" LINK="https://habr.com/ru/post/439576/">
 <edge COLOR="#7c007c"/>
